b22fc6803a1287872413173254f3691ff9a00396
usernamedotmap <zeaxanthin06@gmail.com>
2024-06-18 23:13:11 +0800

    first coomitment

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const mysql = require('mysql')
+const cors = require('cors')
+
+const app = express()
+app.use(cors());
+app.use(express.json())
+
+const db = mysql.createConnection ({
+  host: 'localhost',
+  user: 'root',
+  password: "",
+  database: 'signup'
+})
+
+app.post('/signup', (req, res) => {
+  const sql = "INSERT INTO login (`name`, `email`, `password`) values (?)";
+  const values = [req.body.name, req.body.email, req.body.password]
+  db.query(sql, [values], (err, data) => {
+    if(err) return res.json(err);
+    return res.json(data);
+  })
+})
+
+
+
+app.listen(8000, () => {
+  console.log("Hello");
+})
